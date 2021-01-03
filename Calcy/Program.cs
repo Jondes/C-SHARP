afdff0afd1d871097bfd5b5fa48a2cb8291d0004
johnson guedes <jondes560@hotmail.com>
2021-01-03 19:04:04 +0000

    Calculator
    Calcutor

---
+++ Calcy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Calculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start:
+            int num01;
+            int num02;
+            Console.Write("Enter a number to be divided: ");
+            num01 = Convert.ToInt32( Console.ReadLine());
+            Console.Write("Enter a number to be divide by: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+
+            int v = (num01 / num02);
+            Console.WriteLine(num01 + " divided by " + num02 + " is equal to  " + v);
+
+            // wait for user to press a key
+            Console.ReadKey();
+            Console.WriteLine();
+            goto Start; // Jump to start
+        }
+    }
+}
